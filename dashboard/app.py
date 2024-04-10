16b27bad9f55d7890e642696ad3237743846bf0d
reedbc1 <101766219+reedbc1@users.noreply.github.com>
2024-04-09 22:53:06 -0500

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,101 +1,206 @@
+# --------------------------------------------
+# Imports at the top - PyShiny EXPRESS VERSION
+# --------------------------------------------
+
+# From shiny, import just reactive and render
+from shiny import reactive, render
+
+# From shiny.express, import just ui and inputs if needed
+from shiny.express import ui
+
+import random
+from datetime import datetime
+from collections import deque
+import pandas as pd
 import plotly.express as px
-import seaborn as sns
-from shiny import reactive, render, req
-from shiny.express import input, ui
 from shinywidgets import render_plotly
-from shinywidgets import render_widget
-import palmerpenguins
+from scipy import stats
 
-# Use the built-in function to load the Palmer Penguins dataset
-penguins = palmerpenguins.load_penguins()
+# --------------------------------------------
+# Import icons as you like
+# --------------------------------------------
 
-ui.page_opts(title="Penguin Data - Brendan", fillable=True)
+# https://fontawesome.com/v4/cheatsheet/
+from faicons import icon_svg
 
-# sidebar
-with ui.sidebar(bg="#f8f8f8", open='open'):
-    ui.h2("Sidebar")
-    ui.input_selectize("selected_attribute", "Selected_attribute",
-                       ["bill_length_mm", "bill_depth_mm", 
-                        "flipper_length_mm", "body_mass_g"])
-    ui.input_numeric("plotly_bin_count", "Plotly_bin_count", 50)
-    ui.input_slider("seaborn_bin_count", "Seaborn_bin_count", 1, 100, 50)
-    ui.input_checkbox_group("selected_species_list", "Selected_species_list", 
-                            ["Adelie", "Gentoo", "Chinstrap"], 
-                            selected = ["Adelie", "Gentoo", "Chinstrap"], inline = False)
-    ui.input_checkbox_group("selected_island_list", "Selected_island_list", 
-                            ['Torgersen', 'Biscoe', 'Dream'], 
-                            selected = ['Torgersen', 'Biscoe', 'Dream'], inline = False)
+# --------------------------------------------
+# Shiny EXPRESS VERSION
+# --------------------------------------------
+
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+
+UPDATE_INTERVAL_SECS: int = 3
+
+# --------------------------------------------
+# Initialize a REACTIVE VALUE with a common data structure
+# The reactive value is used to store state (information)
+# Used by all the display components that show this live data.
+# This reactive value is a wrapper around a DEQUE of readings
+# --------------------------------------------
+
+DEQUE_SIZE: int = 5
+reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
+
+# --------------------------------------------
+# Initialize a REACTIVE CALC that all display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+# It returns a tuple with everything needed to display the data.
+# Very easy to expand or modify.
+# --------------------------------------------
+
+
+@reactive.calc()
+def reactive_calc_combined():
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic
+    temp = round(random.uniform(-18, -16), 1)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    new_dictionary_entry = {"temp":temp, "timestamp":timestamp}
+
+    # get the deque and append the new entry
+    reactive_value_wrapper.get().append(new_dictionary_entry)
+
+    # Get a snapshot of the current deque for any further processing
+    deque_snapshot = reactive_value_wrapper.get()
+
+    # For Display: Convert deque to DataFrame for display
+    df = pd.DataFrame(deque_snapshot)
+
+    # For Display: Get the latest dictionary entry
+    latest_dictionary_entry = new_dictionary_entry
+
+    # Return a tuple with everything we need
+    # Every time we call this function, we'll get all these values
+    return deque_snapshot, df, latest_dictionary_entry
+
+
+
+
+# Define the Shiny UI Page layout
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
+ui.page_opts(title="PyShiny Express: Live Data Example", fillable=True)
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+with ui.sidebar(open="open"):
+
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
     ui.hr()
-    ui.a("GitHub", href = "https://github.com/reedbc1/cintel-02-data/tree/main", target = "_blank")
+    ui.h6("Links:")
+    ui.a(
+        "GitHub Source",
+        href="https://github.com/denisecase/cintel-05-cintel",
+        target="_blank",
+    )
+    ui.a(
+        "GitHub App",
+        href="https://denisecase.github.io/cintel-05-cintel/",
+        target="_blank",
+    )
+    ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
+    ui.a(
+        "PyShiny Express",
+        href="hhttps://shiny.posit.co/blog/posts/shiny-express/",
+        target="_blank",
+    )
 
+# In Shiny Express, everything not in the sidebar is in the main panel
 
-# create two columns
 with ui.layout_columns():
-    
-    # show data
-    with ui.navset_card_pill(id="tab1"):  
-        with ui.nav_panel("Data Table"):
-            @render.data_frame  
-            def penguins_data_table():
-                return render.DataTable(penguins)
-        with ui.nav_panel("Data Grid"):
-            @render.data_frame  
-            def penguins_data_grid():
-                return render.DataGrid(penguins)
-                
-    # show graphs
-    with ui.navset_card_pill(id="tab2"):  
+    with ui.value_box(
+        showcase=icon_svg("sun"),
+        theme="bg-gradient-blue-purple",
+    ):
+
+        "Current Temperature"
+
+        @render.text
+        def display_temp():
+            """Get the latest reading and return a temperature string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['temp']} C"
+
+        "warmer than usual"
+
+  
+
+    with ui.card(full_screen=True):
+        ui.card_header("Current Date and Time")
+
+        @render.text
+        def display_time():
+            """Get the latest reading and return a timestamp string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['timestamp']}"
+
+
+#with ui.card(full_screen=True, min_height="40%"):
+with ui.card(full_screen=True):
+    ui.card_header("Most Recent Readings")
+
+    @render.data_frame
+    def display_df():
+        """Get the latest reading and return a dataframe with current readings"""
+        deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+        pd.set_option('display.width', None)        # Use maximum width
+        return render.DataGrid( df,width="100%")
+
+with ui.card():
+    ui.card_header("Chart with Current Trend")
+
+    @render_plotly
+    def display_plot():
+        # Fetch from the reactive calc function
+        deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+
+        # Ensure the DataFrame is not empty before plotting
+        if not df.empty:
+            # Convert the 'timestamp' column to datetime for better plotting
+            df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+            # Create scatter plot for readings
+            # pass in the df, the name of the x column, the name of the y column,
+            # and more
         
-        with ui.nav_panel("Plotly Histogram"):
-            @render_widget  
-            def plot_hist():  
-                mass_fig = px.histogram(
-                    data_frame=filtered_data(),
-                    x=input.selected_attribute(),
-                    color = "species",
-                    nbins=input.plotly_bin_count(),
-                ).update_layout(
-                    title={"text": "Penguin Stats", "x": 0.5},
-                    yaxis_title="Count",
-                    xaxis_title=input.selected_attribute(),
-                )
-                return mass_fig
-        
-        with ui.nav_panel("Seaborn Histogram"):
-            @render.plot(alt="A Seaborn histogram on penguin body mass in grams.")  
-            def plot():  
-                ax = sns.histplot(data=filtered_data(), x="body_mass_g", 
-                                  hue = "species", bins=input.seaborn_bin_count())  
-                ax.set_title("Palmer Penguins")
-                ax.set_xlabel("Mass (g)")
-                ax.set_ylabel("Count")
-                return ax 
+            fig = px.scatter(df,
+            x="timestamp",
+            y="temp",
+            title="Temperature Readings with Regression Line",
+            labels={"temp": "Temperature (°C)", "timestamp": "Time"},
+            color_discrete_sequence=["blue"] )
+            
+            # Linear regression - we need to get a list of the
+            # Independent variable x values (time) and the
+            # Dependent variable y values (temp)
+            # then, it's pretty easy using scipy.stats.linregress()
 
-        with ui.nav_panel("Scatterplot"):
-            @render_widget  
-            def plot_scatter():  
-                plotly_fig = px.scatter(
-                    data_frame=filtered_data(),
-                    x="body_mass_g",
-                    color = "species"
-                ).update_layout(
-                    title={"text": "Penguin Mass", "x": 0.5},
-                    yaxis_title="Count",
-                    xaxis_title="Body Mass (g)",
-                )
-    
-                return plotly_fig  
+            # For x let's generate a sequence of integers from 0 to len(df)
+            sequence = range(len(df))
+            x_vals = list(sequence)
+            y_vals = df["temp"]
 
-# --------------------------------------------------------
-# Reactive calculations and effects
-# --------------------------------------------------------
+            slope, intercept, r_value, p_value, std_err = stats.linregress(x_vals, y_vals)
+            df['best_fit_line'] = [slope * x + intercept for x in x_vals]
 
-@reactive.calc
-def filtered_data():
-    req(input.selected_species_list())
-    req(input.selected_island_list())
-    
-    isSpeciesMatch = penguins["species"].isin(input.selected_species_list())
-    isIslandMatch = penguins["island"].isin(input.selected_island_list())
-    filtered_df_and = penguins[isSpeciesMatch & isIslandMatch]
-    return filtered_df_and
+            # Add the regression line to the figure
+            fig.add_scatter(x=df["timestamp"], y=df['best_fit_line'], mode='lines', name='Regression Line')
+
+            # Update layout as needed to customize further
+            fig.update_layout(xaxis_title="Time",yaxis_title="Temperature (°C)")
+
+            return fig
